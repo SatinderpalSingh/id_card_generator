f3f0d7f23ab3421cfacea0240464131d4feff408
Kunwar-bir-singh <144337681+Kunwar-bir-singh@users.noreply.github.com>
2024-02-19 22:45:24 +0530

    Update initialize.php
    Changing The Password and The Username of the database to the orginals.

--- initialize.php
+++ initialize.php
@@ -5,7 +5,7 @@ if(!defined('base_url')) define('base_url','http://103.66.206.230/icard/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"id_generator");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"RgjW9ytg0rAD6q2g");
 if(!defined('DB_NAME')) define('DB_NAME',"id_generator");
 ?>
